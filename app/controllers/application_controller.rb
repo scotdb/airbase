02f7988bb11b938ab5a855686666206c166d5deb
Philip Nelson <teamdba@scotdb.com>
2010-11-19 21:36:49 +0000

    Start locking down the application via Devise functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :authorize
+
+  protected
+    def authorize
+       # unless signed_in/
+       # flash "Please sign in"
+       redirect_to new_user_session_path
+    end
 end
